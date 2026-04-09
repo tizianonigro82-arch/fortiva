9d3f0b501625254e2a53ce20b0282f33aed89ce0
tizianonigro82-arch <tizianonigro82@gmail.com>
2026-04-09 19:25:14 -0300

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,18 @@
+const Index = () => {
+  return (
+    <iframe
+      src="/fortiva_4.html"
+      title="Fortiva"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+        position: "fixed",
+        top: 0,
+        left: 0,
+      }}
+    />
+  );
+};
+
+export default Index;
